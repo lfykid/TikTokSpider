691047d11e3138d8db8abd526c1eb93e470fa4d0
huangke <huangke@samsource.cn>
2018-09-07 18:01:41 +0800

    添加保存文件夹

--- dyspider.py
+++ dyspider.py
@@ -1,26 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re, os
+
 import requests
 
 url_list = []
 
 def get_all_video_urls(max_cursor):
+    url = "https://www.amemv.com/aweme/v1/aweme/post/"
+    querystring = {
+        "user_id": "99311023790",
+        "count":   "21", "max_cursor": max_cursor,
+        "dytk":    "2f6120d834fc42936c8ae5777546f6b5"
+    }
+    headers = {
+        'accept-encoding':  "gzip, deflate, br",
+        'accept-language':  "zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6",
+        'user-agent':       "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+        'accept':           "application/json",
+        'referer':          "https://www.amemv.com/share/user/99311023790?u_code=md2m918k&timestamp=1536311519&utm_source=qq&utm_campaign=client_share&utm_medium=android&app=aweme&iid=43539176723",
+        'authority':        "www.amemv.com",
+        'x-requested-with': "XMLHttpRequest",
+        'Cache-Control':    "no-cache",
+        'Postman-Token':    "a3873c36-c20c-45f3-baf9-d80c6fdae811"
+    }
     try:
-        url = "https://www.amemv.com/aweme/v1/aweme/post/"
-
-        querystring = {"user_id":    "保密", "count": "21", "max_cursor": max_cursor, "aid": "1128",
-                       "_signature": "保密", "dytk": "保密"}
-        headers = {
-            'cookie':           "保密",
-            'accept-encoding':  "gzip, deflate, br",
-            'accept-language':  "zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6",
-            'user-agent':       "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, "
-                                "like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
-            'accept':           "application/json",
-            'referer':          "https://www.amemv.com/share/user/68152168500",
-            'authority':        "www.amemv.com",
-            'x-requested-with': "XMLHttpRequest",
-            'Cache-Control':    "no-cache",
-            'Postman-Token':    "293ca736-a949-4191-936a-64421bb38c17"
-        }
 
         global url_list
         response = requests.request("GET", url, headers=headers, params=querystring)
@@ -44,7 +49,7 @@ def get_all_video_urls(max_cursor):
         print('failed,', e)
         return None
 
-def download_video(name, url):
+def download_video(username, name, url):
     try:
         headers = {
             'Connection':                "keep-alive",
@@ -63,26 +68,44 @@ def download_video(name, url):
 
         video_response = requests.get(video_url, headers=headers)
         video_data = video_response.content
-        save_video(name, video_data)
+        save_video(username, name, video_data)
     except Exception as e:
         print('download failed,', e)
         return None
 
-def save_video(name, data):
+def save_video(username, name, data):
     if data:
-        with open('%s.mp4' % name, 'wb') as f:
+        with open('%s/%s.mp4' % (username, name), 'wb') as f:
             f.write(data)
             f.close()
     else:
         return
 
-def main():
+def get_name(num):
+    url = "https://www.amemv.com/share/user/%s" % num
+    headers = {
+        'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36"}
+
+    response = requests.request("GET", url, headers=headers)
+    name = re.findall('<p class="nickname">(.*?)</p>', response.text)[0]
+    return name
+
+def makedir(name):
+    if not os.path.isdir(name):
+        os.mkdir(name)
+    else:
+        pass
+
+def main(_id):
+    username = get_name(_id)
+    makedir(username)
     get_all_video_urls(0)
     for item in url_list:
         name = item[0]
         url = item[1]
         print("正在下载: ", name)
-        download_video(name, url)
+        download_video(username, name, url)
 
 if __name__ == '__main__':
-    main()
+    _id = int(input('输入id: '))
+    main(_id)
