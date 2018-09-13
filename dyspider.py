cdad245b8d5b068ed7b686a7fae733611342fc48
huangke <huangkwell@163.com>
2018-09-13 21:41:47 +0800

    整理代理，添加注释说明

--- dyspider.py
+++ dyspider.py
@@ -11,22 +11,28 @@ import requests
 
 from head import download_headers, video_headers, Web_UA
 
-URL_LIST = []
-PAGE = 1
+VIDEO_URLS, PAGE = [], 1
 
 
 def get_all_video_urls(user_id, max_cursor, dytk):
-    global URL_LIST
-    url = "https://www.amemv.com/aweme/v1/aweme/post/?user_id={}&count=21&" \
-          "max_cursor={}&dytk={}".format(user_id, max_cursor, dytk)
+    '''
+    获取所有视频的源地址
+    '''
+    url = "https://www.amemv.com/aweme/v1/aweme/post/?"
+    params = {'user_id'   : user_id,
+              'count'     : 21,
+              'max_cursor': max_cursor,
+              'dytk'      : dytk}
     try:
-        response = requests.request("GET", url, headers=video_headers)
+        response = requests.request("GET", url, params=params, headers=video_headers)
         if response.status_code == 200:
             data = response.json()
             for li in data['aweme_list']:
                 name = li.get('share_info').get('share_desc')
                 url = li.get('video').get('play_addr').get('url_list')[0]
-                URL_LIST.append([name, url])
+                VIDEO_URLS.append([name, url])
+
+            # 下拉获取更多视频
             if data['has_more'] == 1 and data.get('max_cursor') != 0:
                 sleep(1)
                 global PAGE
@@ -37,28 +43,33 @@ def get_all_video_urls(user_id, max_cursor, dytk):
                 return
         else:
             print(response.status_code)
-            return None
+            return
     except Exception as e:
         print('failed,', e)
-        return None
+        return
 
 
 def download_video(index, username, name, url, retry=3):
+    ''' 下载视频数据并显示下载进度 '''
     print("\r正在下载第%s个视频: %s" % (index, name))
     try:
         response = requests.get(url, stream=True, headers=download_headers, timeout=15, allow_redirects=False)
         video_url = response.headers['Location']
-        video_response = requests.get(video_url, headers=download_headers)
+        video_response = requests.get(video_url, headers=download_headers, timeout=15)
+
+        # 重试3次
         if not video_response.status_code == 200:
             retry -= 1
             download_video(index, username, name, url, retry)
         video_size = int(video_response.headers['Content-Length'])
+
+        # 保存视频，显示下载进度
         with open('%s/%s.mp4' % (username, name), 'wb') as f:
-            dl = 0
+            data_length = 0
             for data in video_response.iter_content(chunk_size=1024):
-                dl += len(data)
+                data_length += len(data)
                 f.write(data)
-                done = int(50 * dl / video_size)
+                done = int(50 * data_length / video_size)
                 sys.stdout.write("\r下载进度: [%s%s]" % ('█' * done, ' ' * (50 - done)))
                 sys.stdout.flush()
     except Exception as e:
@@ -67,15 +78,24 @@ def download_video(index, username, name, url, retry=3):
 
 
 def get_name_and_dytk(num):
+    '''
+    获取用户名和dytk参数
+    '''
     url = "https://www.amemv.com/share/user/%s" % num
     headers = {'user-agent': Web_UA}
-    response = requests.request("GET", url, headers=headers)
-    name = re.findall('<p class="nickname">(.*?)</p>', response.text)[0]
-    dytk = re.findall("dytk: '(.*?)'", response.text)[0]
-    return name, dytk
+    try:
+        response = requests.request("GET", url, headers=headers)
+        name = re.findall('<p class="nickname">(.*?)</p>', response.text)[0]
+        dytk = re.findall("dytk: '(.*?)'", response.text)[0]
+        return name, dytk
+    except Exception:
+        return
 
 
 def makedir(name):
+    '''
+     建立以用户名字为名的文件夹
+     '''
     if not os.path.isdir(name):
         os.mkdir(name)
     else:
@@ -83,18 +103,22 @@ def makedir(name):
 
 
 def get_parser():
+    '''
+    解析参数
+    '''
     parser = ArgumentParser()
     parser.add_argument('--uid', dest='user_id', type=int, help='用户的抖音id')
     return parser.parse_args()
 
 
 def main():
+    ''' 主函数 '''
     args = get_parser()
     _id = args.user_id if args.user_id else int(input('请输入你要爬取的抖音用户id: '))
     username, dytk = get_name_and_dytk(_id)
     makedir(username)
     get_all_video_urls(_id, 0, dytk)
-    for index, item in enumerate(URL_LIST, 1):
+    for index, item in enumerate(VIDEO_URLS, 1):
         name = item[0]
         if name == '抖音-原创音乐短视频社区':
             name = name + str(index)
